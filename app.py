c872321985c83e475a15bed24f075ed351865c8e
maxblunck <maxbl@me.com>
2021-03-07 19:21:03 +0100

    actually use gunicorn

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 colors = {
     'background': '#111111',
     'text': '#4264ed'
